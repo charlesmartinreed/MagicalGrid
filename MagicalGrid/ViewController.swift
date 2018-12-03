16bdb11ac598973e0906b564c7d8f371f943fd60
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-02 23:19:48 -0600

    Create full-screen grid.

--- MagicalGrid/ViewController.swift
+++ MagicalGrid/ViewController.swift
@@ -12,9 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //MARK:- Num of boxes on screen logic
+        let numViewPerRow = 15
+        let height = Int(view.frame.height) / numViewPerRow
+        let width = Int(view.frame.width) / numViewPerRow
+        
+        //loop for rendering out more boxes
+        for j in 0...height {
+            for i in 0...numViewPerRow {
+                let cellView = UIView()
+                cellView.backgroundColor = randomColor()
+                cellView.frame = CGRect(x: i * width, y: j * width, width: width, height: width) //starting in top-left
+                cellView.layer.borderWidth = 0.5
+                cellView.layer.borderColor = UIColor.black.cgColor
+                view.addSubview(cellView)
+            }
+        }
     }
 
+    fileprivate func randomColor() -> UIColor {
+        
+        let red = CGFloat(drand48()) //a double, random number from 0-1
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1)
+    }
 
 }
 
